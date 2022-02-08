15cbbfcbe6d9e8de92f0c82edca4c8abeb6a138c
Zwettekop <zwartekop2@gmail.com>
2022-02-09 00:47:11 +0100

    added option to enter braille directly

--- script.js
+++ script.js
@@ -1,136 +1,57 @@
 let btn = document.getElementById("btnConvert");
-let iptVeld = document.getElementById("iptTekst");
-let iptOutput = document.getElementById("iptBraille");
+let iptText = document.getElementById("iptTekst");
+let iptBraille = document.getElementById("iptBraille");
 let svg = document.getElementById("svgBraille");
 //⠇⠏⠤⠑⠂⠶
-textToShape = {
-    " ": " ",
-    "a": "1",
-    "b": "12",
-    "c": "14",
-    "d": "145",
-    "e": "15",
-    "f": "124",
-    "g": "1245",
-    "h": "125",
-    "i": "24",
-    "j": "245",
-    "k": "13",
-    "l": "123",
-    "m": "134",
-    "n": "1345",
-    "o": "135",
-    "p": "1234",
-    "q": "12345",
-    "r": "1235",
-    "s": "234",
-    "t": "2345",
-    "u": "136",
-    "v": "1236",
-    "w": "2456",
-    "x": "1346",
-    "y": "13456",
-    "z": "1356",
-    "#": "3456",
-    "1": "1",
-    "2": "12",
-    "3": "14",
-    "4": "145",
-    "5": "15",
-    "6": "124",
-    "7": "1245",
-    "8": "125",
-    "9": "24",
-    "0": "245",
-    ",": "2",
-    ":": "25",
-    ".": "256",
-    "?": "236",
-    "!": "235",
-    "(": "5 126",
-    ")": "5 345",
-    "/": "456 34",
-    "": "456 16",
-    "-": "36"
-}
+brailleToShape = { " ": " ", "⠁": "1", "⠃": "12", "⠉": "14", "⠙": "145", "⠑": "15", "⠋": "124", "⠛": "1245", "⠓": "125", "⠊": "24", "⠚": "245", "⠅": "13", "⠇": "123", "⠍": "134", "⠝": "1345", "⠕": "135", "⠏": "1234", "⠟": "12345", "⠗": "1235", "⠎": "234", "⠞": "2345", "⠥": "136", "⠧": "1236", "⠺": "2456", "⠭": "1346", "⠽": "13456", "⠵": "1356", "⠼": "3456", "⠁": "1", "⠃": "12", "⠉": "14", "⠙": "145", "⠑": "15", "⠋": "124", "⠛": "1245", "⠓": "125", "⠊": "24", "⠚": "245", "⠂": "2", "⠒": "25", "⠲": "256", "⠦": "236", "⠖": "235", "⠐⠣": "5 126", "⠐⠜": "5 345", "⠸⠌": "456 34", "⠸⠡": "456 16", "⠤": "36", };
 
-textToBraille = {
-    " ": " ",
-    "a": "⠁",
-    "b": "⠃",
-    "c": "⠉",
-    "d": "⠙",
-    "e": "⠑",
-    "f": "⠋",
-    "g": "⠛",
-    "h": "⠓",
-    "i": "⠊",
-    "j": "⠚",
-    "k": "⠅",
-    "l": "⠇",
-    "m": "⠍",
-    "n": "⠝",
-    "o": "⠕",
-    "p": "⠏",
-    "q": "⠟",
-    "r": "⠗",
-    "s": "⠎",
-    "t": "⠞",
-    "u": "⠥",
-    "v": "⠧",
-    "w": "⠺",
-    "x": "⠭",
-    "y": "⠽",
-    "z": "⠵",
-    "#": "⠼",
-    "1": "⠁",
-    "2": "⠃",
-    "3": "⠉",
-    "4": "⠙",
-    "5": "⠑",
-    "6": "⠋",
-    "7": "⠛",
-    "8": "⠓",
-    "9": "⠊",
-    "0": "⠚",
-    ",": "⠂",
-    ":": "⠒",
-    ".": "⠲",
-    "?": "⠦",
-    "!": "⠖",
-    "(": "⠐⠣",
-    ")": "⠐⠜",
-    "/": "⠸⠌",
-    "": "⠸⠡",
-    "-": "⠤"
-}
+textToBraille = { "a": "⠁", "b": "⠃", "c": "⠉", "d": "⠙", "e": "⠑", "f": "⠋", "g": "⠛", "h": "⠓", "i": "⠊", "j": "⠚", "k": "⠅", "l": "⠇", "m": "⠍", "n": "⠝", "o": "⠕", "p": "⠏", "q": "⠟", "r": "⠗", "s": "⠎", "t": "⠞", "u": "⠥", "v": "⠧", "w": "⠺", "x": "⠭", "y": "⠽", "z": "⠵", "#": "⠼", "1": "⠁", "2": "⠃", "3": "⠉", "4": "⠙", "5": "⠑", "6": "⠋", "7": "⠛", "8": "⠓", "9": "⠊", "0": "⠚", ",": "⠂", ":": "⠒", ".": "⠲", "?": "⠦", "!": "⠖", "(": "⠐⠣", ")": "⠐⠜", "/": "⠸⠌", "": "⠸⠡", "-": "⠤" };
 
-cleanInput();
-iptVeld.onchange = cleanInput;
-iptVeld.onkeyup = cleanInput;
-iptVeld.onclick = cleanInput;
-iptVeld.onpaste = cleanInput;
+//* Add event listeners
+iptText.onchange = generateBrailleTekst;
+iptText.onkeyup = generateBrailleTekst;
+iptText.onclick = generateBrailleTekst;
+iptText.onpaste = generateBrailleTekst;
 
-function cleanInput() {
-    let invoer = iptVeld.value.toLowerCase();
+iptBraille.onchange = generateSvg;
+iptBraille.onkeyup = generateSvg;
+iptBraille.onclick = generateSvg;
+iptBraille.onpaste = generateSvg;
+
+//* Add code
+generateBrailleTekst();
+
+function generateBrailleTekst() {
+    //Clean input 
+    let invoer = iptText.value.toLowerCase();
     let res = "";
     for (let c of invoer) {
         if (textToBraille.hasOwnProperty(c))
             res += c;
     }
-    console.log(res);
-    generateBrailleTekst(res);
-    generateSvg(res);
-}
-
-function generateBrailleTekst(invoer) {
-    let res = "";
+    //console.log(res);
+    //Translate
+    invoer = res;
+    res = "";
     for (let c of invoer)
         res += textToBraille[c];
-    iptOutput.value = res;
+    iptBraille.value = res;
+    //! Not a great idea
+    generateSvg();
 }
 
-function generateSvg(invoer) {
+
+
+function generateSvg() {
+    //Clean input
+    invoer = iptBraille.value;
+    res = "";
+    for (let c of invoer)
+        if (brailleToShape.hasOwnProperty(c))
+            res += c;
+
+    invoer = res;
+    //console.log(invoer);
     svg.innerHTML = "";
 
     let x = 50;
@@ -141,14 +62,14 @@ function generateSvg(invoer) {
 
     for (let c of invoer) {
         //console.log(`${c} => ${textToShape[c]}`);
-        let shape = textToShape[c];
+        let shape = brailleToShape[c];
         //Draw character
         for (let dot of shape) {
             if (c === " ")
                 break;
             dot = parseInt(dot);
             let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-            circle.setAttribute("fill", "black");
+            circle.setAttribute("fill", "#6666FF");
             let cx = dot > 3 ? dotSpacing : 0;
             let cy = ((dot - 1) % 3) * dotSpacing;
             circle.setAttribute("cx", x + cx);
@@ -156,7 +77,6 @@ function generateSvg(invoer) {
             circle.setAttribute("r", dotSize);
             svg.appendChild(circle);
         }
-
         x += letterSpacing;
     }
 }
--- test.js
+++ test.js
@@ -1,52 +1,3 @@
-/*let invoer = `a;1
-b;12
-c;14
-d;145
-e;15
-f;124
-g;1245
-h;125
-i;24
-j;245
-k;13
-l;123
-m;134
-n;1345
-o;135
-p;1234
-q;12345
-r;1235
-s;234
-t;2345
-u;136
-v;1236
-w;2456
-x;1346
-y;13456
-z;1356
-number indicator;3456
-1;1
-2;12
-3;14
-4;145
-5;15
-6;124
-7;1245
-8;125
-9;24
-0;245
-,;2
-:;25
-.;256
-?;236
-!;235
-(;5 126
-);5 345
-/;456 34
-\;456 16
--;36`;*/
-
-
 let invoer = `a;⠁
 b;⠃
 c;⠉
@@ -73,7 +24,7 @@ w;⠺
 x;⠭
 y;⠽
 z;⠵
-number indicator;⠼
+#;⠼
 1;⠁
 2;⠃
 3;⠉
@@ -95,15 +46,12 @@ number indicator;⠼
 \;⠸⠡
 -;⠤`
 
-/*
+
 arr = invoer.split("\n");
-dict = {};
+res = ""
 for (let zin of arr) {
     woorden = zin.split(";");
     //console.log(woorden);
-    dict[woorden[0]] = woorden[1];
-    console.log(`"${woorden[0]}":"${woorden[1]}",`);
+    res += `"${woorden[0]}":"${woorden[1]}",`;
 }
-
-*/
-//console.log(dict); 
+console.log(res);
